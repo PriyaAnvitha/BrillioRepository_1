68be104517d74c522f4affdf82ff333e4148cb78
priya <priyaanvitha1155@gmail.com>
2022-12-02 17:57:49 +0530

    Spring boot demo

---
+++ temp6/src/main/java/com/example/temp6/Temp6Application.java
@@ -0,0 +1,13 @@
+package com.example.temp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Temp6Application.class, args);
+	}
+
+}
---
+++ temp6/src/test/java/com/example/temp6/Temp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Temp6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
